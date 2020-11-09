7d708788eaefcd0bc4f05685c5e7f847e69c65d2
unknown <katasiw97@gmail.com>
2020-11-09 16:44:44 +0100

    Testing more exceptions

--- zad1.py
+++ zad1.py
@@ -21,7 +21,36 @@ class Hamming:
           File "C:/Users/Katarzyna/Desktop/3 semestr/Testowanie Automatyczne/lab6/laboratorium-6-cati97/zad1.py", line 27, in distance
             raise ValueError("Strands must have the same length")
         ValueError: Strands must have the same length
+        >>> ham.distance("ATA", "AGTG")
+        Traceback (most recent call last):
+          File "C:\\Users\\Katarzyna\\AppData\\Local\\Programs\\Python\\Python36\\lib\\doctest.py", line 1330, in __run
+            compileflags, 1), test.globs)
+          File "<doctest __main__.Hamming.distance[6]>", line 1, in <module>
+            ham.distance("ATA", "AGTG")
+          File "C:/Users/Katarzyna/Desktop/3 semestr/Testowanie Automatyczne/lab6/laboratorium-6-cati97/zad1.py", line 27, in distance
+            raise ValueError("Strands must have the same length")
+        ValueError: Strands must have the same length
+        >>> ham.distance("", "G")
+        Traceback (most recent call last):
+          File "C:\\Users\\Katarzyna\\AppData\\Local\\Programs\\Python\\Python36\\lib\\doctest.py", line 1330, in __run
+            compileflags, 1), test.globs)
+          File "<doctest __main__.Hamming.distance[6]>", line 1, in <module>
+            ham.distance("", "G")
+          File "C:/Users/Katarzyna/Desktop/3 semestr/Testowanie Automatyczne/lab6/laboratorium-6-cati97/zad1.py", line 27, in distance
+            raise ValueError("Strands must have the same length")
+        ValueError: Strands must have the same length
+        >>> ham.distance("G", "")
+        Traceback (most recent call last):
+          File "C:\\Users\\Katarzyna\\AppData\\Local\\Programs\\Python\\Python36\\lib\\doctest.py", line 1330, in __run
+            compileflags, 1), test.globs)
+          File "<doctest __main__.Hamming.distance[6]>", line 1, in <module>
+            ham.distance("G", "")
+          File "C:/Users/Katarzyna/Desktop/3 semestr/Testowanie Automatyczne/lab6/laboratorium-6-cati97/zad1.py", line 27, in distance
+            raise ValueError("Strands must have the same length")
+        ValueError: Strands must have the same length
         """
+
+
         if len(first) == len(second):
             count_diff = 0
             for i in range(len(first)):
