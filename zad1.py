b37a750f7189c70c86d9861b85bb755db848e630
unknown <katasiw97@gmail.com>
2020-11-09 16:37:10 +0100

    Testing exception works

--- zad1.py
+++ zad1.py
@@ -12,6 +12,15 @@ class Hamming:
         0
         >>> ham.distance("GGACGGATTCTG", "AGGACGGATTCT")
         9
+        >>> ham.distance("AATG", "AAA")
+        Traceback (most recent call last):
+          File "C:\\Users\\Katarzyna\\AppData\\Local\\Programs\\Python\\Python36\\lib\\doctest.py", line 1330, in __run
+            compileflags, 1), test.globs)
+          File "<doctest __main__.Hamming.distance[6]>", line 1, in <module>
+            ham.distance("AATG", "AAA")
+          File "C:/Users/Katarzyna/Desktop/3 semestr/Testowanie Automatyczne/lab6/laboratorium-6-cati97/zad1.py", line 27, in distance
+            raise ValueError("Strands must have the same length")
+        ValueError: Strands must have the same length
         """
         if len(first) == len(second):
             count_diff = 0
